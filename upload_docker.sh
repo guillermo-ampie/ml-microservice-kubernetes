c39b4fadfd6e448974e158478db495db67611329
Guillermo Ampie <ga.ampie@gmail.com>
2021-02-23 17:18:48 -0600

    Complete upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -2,14 +2,21 @@
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
+DOCKER_HUB_ID="gampie"
+DOCKER_REPOSITORY="housing-prices-api"
+VERSION="1.0"
 
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=${DOCKER_HUB_ID}/${DOCKER_REPOSITORY}
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u ${DOCKER_HUB_ID}
+docker tag ${DOCKER_REPOSITORY}:${VERSION} ${dockerpath}:${VERSION}
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}:${VERSION}
