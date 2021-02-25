3ce6edca3b561ef4217b98e38261013db9790d13
Guillermo Ampie <ga.ampie@gmail.com>
2021-02-25 17:05:37 -0600

    Use variables in run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+CONTAINER_NAME="housing-prices-api"
+VERSION=1.0
+CONTAINER_PORT=80
+HOST_PORT=8000
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag housing-prices-api:1.0 .
+docker build --tag ${CONTAINER_NAME}:${VERSION} .
 
 # Step 2:
 # List docker images
@@ -12,4 +16,4 @@ docker images
 
 # Step 3:
 # Run flask app
-docker run -p 8000:80 housing-prices-api:1.0
+docker run -p ${HOST_PORT}:${CONTAINER_PORT} ${CONTAINER_NAME}:${VERSION}
